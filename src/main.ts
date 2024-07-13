d988e5c533b5246b061d6ec4c4dd2d678297aae1
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-07-13 20:10:28 +0200

    Add Room class to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import Canvas from "./canvas/canvas";
 import { connectControlPanel } from "./panel";
 import Subject from "./subject/subject";
+import Room from "./map/room";
 
 const canvas = new Canvas(document.querySelector("#canvas") as HTMLCanvasElement);
 const subject = new Subject(canvas);
+const room = new Room(canvas);
 connectControlPanel(subject);
