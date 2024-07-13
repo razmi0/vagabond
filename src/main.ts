c404c7acac24bfc4736b85cce540d9a149a6d462
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-07-13 15:20:58 +0200

    Setup starting conditions

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import Canvas from "./classes/canvas";
+import Canvas from "./canvas/canvas";
+import { connectControlPanel } from "./panel";
+import Subject from "./subject/subject";
 
-const canvas = new Canvas(document.getElementById("canvas") as HTMLCanvasElement);
-canvas.setup();
+const canvas = new Canvas(document.querySelector("#canvas") as HTMLCanvasElement);
+const subject = new Subject(canvas);
+connectControlPanel(subject);
