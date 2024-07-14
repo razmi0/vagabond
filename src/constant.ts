bf469dbc073e0e237b1795c307e694acfb0897a5
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-07-14 14:26:43 +0200

    Implement cmds

--- src/constant.ts
+++ src/constant.ts
@@ -10,4 +10,37 @@ export const CONSTANT = {
       dimensions: [window.innerWidth, window.innerHeight] as const,
     },
   },
+  // TODO : Implement the following commands
+  // Implemented : clear
+  maxHistoryCmds: 5,
+  cmds: {
+    help: {
+      description: "List all available commands",
+      usage: "help",
+      example: "help",
+    },
+    list: {
+      description: "List all available commands",
+      usage: "list",
+      example: "list",
+    },
+    clear: {
+      description: "Remove all tasks",
+      usage: "clear",
+      example: "clear",
+      run: (element: HTMLDivElement) => {
+        element.innerHTML = "";
+      },
+    },
+    reset: {
+      description: "Refresh the application",
+      usage: "reset",
+      example: "reset",
+    },
+    genMap: {
+      description: "Generate a map",
+      usage: "genMap",
+      example: "genMap",
+    },
+  },
 } as const;
